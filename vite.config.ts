b608fe52be78d34f9a816556af699199c18489cd
sumanth-github <111294673+sumanth-github@users.noreply.github.com>
2025-08-31 23:56:50 +0530

    package update

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'
+import path from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    include: ['@headlessui/react', '@heroicons/react'], // add this
+    exclude: ['lucide-react'], // keep existing exclude
   },
   resolve: {
     alias: {
